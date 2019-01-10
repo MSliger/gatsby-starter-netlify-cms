b4093fed64e3ec62c660e3d39865182dec3c7cca
Matt Sliger <msliger@globalcloud.net>
2019-01-10 14:20:41 -0500

    fix repo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link } from 'gatsby'
 import logo from '../img/logo.svg'
 
-<<<<<<< HEAD
 const Navbar = class extends React.Component {
 
   componentDidMount() {
@@ -73,33 +72,6 @@ const Navbar = class extends React.Component {
       </div>
       </div>
     </div>
-=======
-const Navbar = () => (
-  <nav className="navbar is-transparent">
-	<div className="container">
-	  <div className="navbar-brand">
-		<Link to="/" className="navbar-item">
-		  <figure className="image">
-			<img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-		  </figure>
-		</Link>
-	  </div>
-	  <div className="navbar-end">
-	  <div className="navbar-item has-dropdown is-hoverable">
-		<a className="navbar-link">
-		More
-		</a>
-			<div className="navbar-dropdown">
-				<Link className="navbar-item" to="/about">About</Link>
-				<Link className="navbar-item" to="/products">Products</Link>
-				<Link className="navbar-item" to="/contact">Contact</Link>
-				<hr className="navbar-divider"/>
-				<a className="navbar-item">Report an issue</a>
-			</div>
-		</div>
-	  </div>
-	</div>
->>>>>>> 47e75678da9562c305407a46b9ce14e80168a48c
   </nav>
   )}
 }
